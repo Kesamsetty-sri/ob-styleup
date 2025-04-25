b84272ebcff88d75cba74da97e30ae58df310b9b
Kesamsetty-sri <116504016+Kesamsetty-sri@users.noreply.github.com>
2025-04-26 02:47:25 +0530

    Update simple_backend_server.js

--- ob-styleup/simple_backend_server.js
+++ ob-styleup/simple_backend_server.js
@@ -79,7 +79,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'fixed_index1_clean.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
